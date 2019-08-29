0b739e2d7025cf4f3fe2379be27d2e727580343f
fansyuri908 <fansyuri72@gmail.com>
2019-08-29 10:18:40 +0700

    Initial commit

---
+++ app/src/main/java/com/FansyuriA/AplicationSholatFansyuri/Kota.kt
@@ -0,0 +1,12 @@
+package com.FansyuriA.AplicationSholatFansyuri
+
+class Kota {
+
+
+
+    var id: Int? = null
+    var nama: String? = null
+    override fun toString(): String {
+        return nama.toString()
+    }
+}
